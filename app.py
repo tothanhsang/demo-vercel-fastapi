d1d73acc23d01cd6044098472ad354f6e3931319
To Thanh Sang <sang.to@coinhako.com>
2022-10-02 02:14:59 +0700

    tst

--- app.py
+++ app.py
@@ -8,34 +8,34 @@ import models
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
-def index(request: Request, username: str = None):
-    if not username:
-        return templates.TemplateResponse("index.html", context={"request": request})
+# @app.get("/", response_class=HTMLResponse)
+# def index(request: Request, username: str = None):
+#     if not username:
+#         return templates.TemplateResponse("index.html", context={"request": request})
 
-    user = get_github_profile(request, username)
+#     user = get_github_profile(request, username)
 
-    context = {"request": request, "user": user}
+#     context = {"request": request, "user": user}
 
-    return templates.TemplateResponse("index.html", context=context)
+#     return templates.TemplateResponse("index.html", context=context)
 
 
-@app.get("/{username}", response_model=models.GithubUserModel)
-def get_github_profile(request: Request, username: str) -> Optional[models.GithubUserModel]:
+# @app.get("/{username}", response_model=models.GithubUserModel)
+# def get_github_profile(request: Request, username: str) -> Optional[models.GithubUserModel]:
 
-    headers = {"accept": "application/vnd.github.v3+json"}
+#     headers = {"accept": "application/vnd.github.v3+json"}
 
-    response = httpx.get(f"https://api.github.com/users/{username}", headers=headers)
+#     response = httpx.get(f"https://api.github.com/users/{username}", headers=headers)
 
-    if response.status_code == 404:
-        return False
+#     if response.status_code == 404:
+#         return False
 
-    user = models.GithubUserModel(**response.json())
+#     user = models.GithubUserModel(**response.json())
 
-    # Sobreescribir la fecha con el formato que necesitamos
-    user.created_at = datetime.strptime(user.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%y")
+#     # Sobreescribir la fecha con el formato que necesitamos
+#     user.created_at = datetime.strptime(user.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%y")
 
-    return user
+#     return user
